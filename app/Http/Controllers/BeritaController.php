968ced2e1ed365e7fa3daa081891452908529c0b
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-18 07:05:53 -0700

    update

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -3,6 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+use Illuminate\Support\Arr;
+use App\Models\Login;
+use App\Models\Pengaduan;
+use App\Models\Kritiksaran;
+use App\Models\Keuangan;
+use App\Models\Surat;
+use App\Models\Berita;
 
 class BeritaController extends Controller
 {
