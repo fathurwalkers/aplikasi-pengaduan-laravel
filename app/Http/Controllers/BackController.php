de69a9236b81448aec44bb1476a8ec6dbee90fd8
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-18 07:08:53 -0700

    update

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -3,8 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+use Illuminate\Support\Arr;
+use App\Models\Login;
+use App\Models\Pengaduan;
+use App\Models\Kritiksaran;
+use App\Models\Keuangan;
+use App\Models\Surat;
+use App\Models\Berita;
 
 class BackController extends Controller
 {
-    //
+    public $users;
+
+    public function __construct()
+    {
+        $session_users = session('data_login');
+        $users = Login::find($session_users->id);
+        $this->users = $users;
+    }
+
+    public function postlogin(Request $request)
+    {
+        $username = $request->login_username;
+        $password = $request->login_password;
+        $data_login = Login::where('login_username', $username)->first();
+        if ($data_login == null) {
+            return redirect()->route('login')->with('status', 'Maaf username yang anda masukkan tidak terdaftar!');
+        }
+        switch ($data_login->login_level) {
+            case 'admin':
+                $cek_password = Hash::check($password, $data_login->login_password);
+                if ($data_login) {
+                    if ($cek_password) {
+                        $users = session(['data_login' => $data_login]);
+                        return redirect()->route('dashboard')->with('status', 'Berhasil Login!');
+                    }
+                }
+                break;
+            case 'user':
+                $cek_password = Hash::check($password, $data_login->login_password);
+                if ($data_login) {
+                    if ($cek_password) {
+                        $users = session(['data_login' => $data_login]);
+                        return redirect()->route('dashboard')->with('status', 'Berhasil Login!');
+                    }
+                }
+                break;
+        }
+        return back()->with('status', 'Maaf password yang anda masukkan salah!')->withInput();
+    }
 }
