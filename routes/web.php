577099aad4378cb5cccf2d249c7a73e2cf08e02c
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-25 01:24:07 -0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BackController;
+use App\Http\Controllers\BeritaController;
 use App\Http\Controllers\KeuanganController;
 use App\Http\Controllers\KritiksaranController;
 use App\Http\Controllers\PengaduanController;
@@ -20,10 +21,18 @@ Route::group(['prefix' => '/dashboard', 'middleware' => 'ceklogin'], function ()
     Route::get('/', [BackController::class, 'index'])->name('dashboard');
 
     // BERITA ROUTE
-    Route::
+    Route::group(['prefix' => '/berita'], function () {
+        Route::get('/', function () {
+            return redirect()->route('daftar-berita');
+        });
+        Route::get('/daftar-berita', [BeritaController::class, 'daftar_berita'])->name('daftar-berita');
+    });
 
     // PENGADUAN ROUTE
     Route::group(['prefix' => '/pengaduan'], function () {
+        Route::get('/', function () {
+            return redirect()->route('pembuatan-pengaduan');
+        });
         Route::get('/pembuatan-pengaduan', [PengaduanController::class, 'pembuatan_pengaduan'])->name('pembuatan-pengaduan');
         Route::post('/post-pembuatan-pengaduan', [PengaduanController::class, 'post_pembuatan_pengaduan'])->name('post-pembuatan-pengaduan');
         Route::post('/konfirmasi-pengaduan', [PengaduanController::class, 'konfirmasi_pengaduan'])->name('konfirmasi-pengaduan');
@@ -31,6 +40,9 @@ Route::group(['prefix' => '/dashboard', 'middleware' => 'ceklogin'], function ()
 
     // KRITIK DAN SARAN ROUTE
     Route::group(['prefix' => '/kritik-dan-saran'], function () {
+        Route::get('/', function () {
+            return redirect()->route('pembuatan-kritiksaran');
+        });
         Route::get('/pembuatan-kritiksaran', [KritiksaranController::class, 'pembuatan_kritiksaran'])->name('pembuatan-kritiksaran');
         Route::post('/post-pembuatan-kritiksaran', [KritiksaranController::class, 'post_pembuatan_kritiksaran'])->name('post-pembuatan-kritiksaran');
     });
