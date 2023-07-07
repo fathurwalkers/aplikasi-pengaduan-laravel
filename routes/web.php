1c3f90f5532da5ff09f792d8030310e92f6ce5fa
Fathur Walkers <fathurwalkers44@gmail.com>
2023-07-07 12:24:59 -0700

    update

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,25 @@ use App\Http\Controllers\{
     HomeController
 };
 
+// Route untuk Halaman Login
 Route::get('/login', [BackController::class, 'login'])->name('login');
-Route::get('/register', [BackController::class, 'register'])->name('register');
+// Route untuk Proses permintaan Login ke Dashboard
 Route::post('/login/proses-login', [BackController::class, 'postlogin'])->name('post-login');
+
+// Route untuk Halaman Register
+Route::get('/register', [BackController::class, 'register'])->name('register');
+// Route untuk Proses pembuatan Akun Baru (Register)
 Route::post('/login/proses-register', [BackController::class, 'postregister'])->name('post-register');
+
+// Route untuk Keluar dari sesi Halaman Dashboard dan Kembali ke Halaman Login
 Route::post('/logout', [BackController::class, 'logout'])->name('logout');
 
+// Route Group untuk HomeController (Halaman Depan Aplikasi)
 Route::group(['prefix' => '/home'], function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
 });
 
+// Route untuk Halaman Dashboard Aplikasi
 Route::group(['prefix' => '/dashboard', 'middleware' => 'ceklogin'], function () {
     Route::get('/', [BackController::class, 'index'])->name('dashboard');
 
@@ -59,6 +68,7 @@ Route::group(['prefix' => '/dashboard', 'middleware' => 'ceklogin'], function ()
     });
 });
 
+// Route Default ketika aplikasi Pertama Kali di Akses akan langsung diarahkan ke Halaman /home
 Route::get('/', function() {
     return redirect()->route('home');
 });
