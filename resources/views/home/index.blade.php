650b5da5b2e80f407a95c9ad2719e4f72f49472f
Fathur Walkers <fathurwalkers44@gmail.com>
2023-07-16 18:40:43 -0700

    update

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -55,7 +55,7 @@
                                 <div class="card-footer">
                                     <small class="text-muted">{{ date('d/m/Y', strtotime($item->berita_tanggal)) }}</small>
                                     <button type="button" class="btn btn-success btn-sm float-right">
-                                        Selengkapnya >>>
+                                        Selengkapnya
                                     </button>
                                 </div>
                             </div>
